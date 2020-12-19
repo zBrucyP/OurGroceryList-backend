268422ced99d5c541a0d3d6b322add40ea99aaa2
zBrucyP <zbrucyp@gmail.com>
2020-12-19 15:05:50 -0600

    update db connection details

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,16 @@
-const monk = require('monk'); // manage tasks with mongo
-const db = monk('localhost/grocerylist');
+const postgres = require('postgres'); // manage tasks with mongo
+
+const options = {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'OurGroceryList',
+    username: 'user',
+    password: 'pass',
+    ssl: false,
+    max: 100,
+    timeout: 10,
+}
+
+const db = postgres(options);
 
 module.exports = db;
