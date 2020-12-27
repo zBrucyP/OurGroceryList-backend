6a2600ceab560e3926d7a22d5607d73d06ddb34e
zBrucyP <zbrucyp@gmail.com>
2020-12-27 09:49:01 -0600

    prettier, log cleanup

--- db/listsDataAccess.js
+++ db/listsDataAccess.js
@@ -1,93 +1,99 @@
 const db = require('../db/connection');
 const CONSTANTS = require('../utils/constants').constants;
 
-
 async function addList(list) {
-    try{
-        const newListCall = await db`
+  try {
+    const newListCall = await db`
             INSERT INTO lists (
                 name, description, owner
             ) VALUES (
                 ${list.name}, ${list.description}, ${list.ownerID}
             )
             returning *
-        `
-        .then((addedList) => {
-            if(addedList && addedList.count === 1) { list.wasSuccessfullyAdded = true }
-        });
-    } catch(e) {
-        const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
-        list.errors.push(error);
-    }
+        `.then((addedList) => {
+      if (addedList && addedList.count === 1) {
+        list.wasSuccessfullyAdded = true;
+      }
+    });
+  } catch (e) {
+    const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
+    list.errors.push(error);
+  }
 }
 
 async function getAllListsForUser(user) {
-    try {
-        const getListsCall = await db`
+  try {
+    const getListsCall = await db`
             SELECT *
             FROM lists
             WHERE owner=${user.id}
         `.then((lists) => {
-            user.getListsWasSuccessful = true;
-            user.lists = lists;
-        })
-    } catch(e) {
-        const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
-        list.errors.push(error);
-    }
+      user.getListsWasSuccessful = true;
+      user.lists = lists;
+    });
+  } catch (e) {
+    const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
+    list.errors.push(error);
+  }
 }
 
 async function deleteList(list) {
-    try {
-        const deleteListCall = await db`
+  try {
+    const deleteListCall = await db`
             DELETE FROM lists
             WHERE id=${list.id}
-        `
-        .then((deletedList) => {
-            if(deletedList.count === 1) { list.wasDeleted = true } 
-        });
-    } catch(e) {
-        const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
-        list.errors.push(error);
-    } 
+        `.then((deletedList) => {
+      if (deletedList.count === 1) {
+        list.wasDeleted = true;
+      }
+    });
+  } catch (e) {
+    const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
+    list.errors.push(error);
+  }
 }
 
 async function getListItems(list) {
-    try {
-        const listItemsCall = await db`
+  try {
+    const listItemsCall = await db`
             SELECT *
             FROM list_items
             WHERE list_id=${list.id}
         `.then((returnedListItems) => {
-            console.log(returnedListItems);
-            list.items = returnedListItems;
-            list.getDetailsSuccessful = true;
-        });
-    } catch(e) {
-        const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
-        list.errors.push(error);
-    } 
+      list.items = returnedListItems;
+      list.getDetailsSuccessful = true;
+    });
+  } catch (e) {
+    const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
+    list.errors.push(error);
+  }
 }
 
 async function addListItems(list) {
-    try {
-        const addItemsCall = await db`
-            insert into list_items ${
-                db(list.listItems, 'list_id', 'name', 'description', 'price', 'quantity', 'bought')
-            }
+  try {
+    const addItemsCall = await db`
+            insert into list_items ${db(
+              list.listItems,
+              'list_id',
+              'name',
+              'description',
+              'price',
+              'quantity',
+              'bought',
+            )}
         `.then((itemsInserted) => {
-            list.itemsAddedSuccessfully = true;
-        });
-    } catch(e) {
-        const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
-        list.errors.push(error);
-    } 
+      list.itemsAddedSuccessfully = true;
+    });
+  } catch (e) {
+    const error = new Error(CONSTANTS.ERROR_DB_CALL_FAILED);
+    list.errors.push(error);
+  }
 }
 
-module.exports = { 
-    addList,
-    getAllListsForUser,
-    deleteList,
-    getListItems,
-    addListItems,
-}
+module.exports = {
+  addList,
+  getAllListsForUser,
+  deleteList,
+  getListItems,
+  addListItems,
+};
--- db/userDataAccess.js
+++ db/userDataAccess.js
@@ -1,71 +1,66 @@
 const db = require('../db/connection');
-const bcrypt = require('bcryptjs'); // was causing node not to launch 
+const bcrypt = require('bcryptjs'); // was causing node not to launch
 const { query } = require('express');
 const e = require('express');
 
 // https://www.npmjs.com/package/postgres
 
-
 async function insertUser(user) {
-    try {
-        const existingUser = await db`
+  try {
+    const existingUser = await db`
             SELECT *
             FROM users
             WHERE email=${user.email}
-        `
-        .then((existingUser) => {
-            if (existingUser.count > 0) {
-                console.log(existingUser);
-                return false;
-            } else {
-                //salt and hash the password
-                bcrypt.hash(user.password, 10).then( async (hash, err) => { 
-                    try {
-                        const new_user = await db`
+        `.then((existingUser) => {
+      if (existingUser.count > 0) {
+        return false;
+      } else {
+        //salt and hash the password
+        bcrypt.hash(user.password, 10).then(async (hash, err) => {
+          try {
+            const new_user = await db`
                             INSERT INTO users (
                                 fname, password, email
                             ) VALUES (
                                 ${user.fname}, ${hash}, ${user.email}
                             )
                             returning *
-                        `
-                        .then((newUser) => {
-                            if(newUser.count === 1) {
-                                user.id = newUser[0].id;
-                                user.userWasCreated = true;
-                            }
-                        });
-                    } catch (e) {
-                        console.log(e);
-                    }
-                });
-            }
+                        `.then((newUser) => {
+              if (newUser.count === 1) {
+                user.id = newUser[0].id;
+                user.userWasCreated = true;
+              }
+            });
+          } catch (e) {
+            console.log(e);
+          }
         });
-    } catch(e) {
-        console.log(e);
-    }
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function findUser (user) {
-    try {
-        const existingUser = await db`
+async function findUser(user) {
+  try {
+    const existingUser = await db`
             SELECT *
             FROM users
             WHERE email=${user.email}
-        `
-        .then((queryResult) => {
-            if (queryResult.count === 1) {
-                user.passwordHashFromDB = queryResult[0].password;
-                user.wasFound = true; 
-                user.fname = queryResult[0].fname;
-            }
-        });
-    } catch (e) {
-        console.log(e);
-    }
+        `.then((queryResult) => {
+      if (queryResult.count === 1) {
+        user.passwordHashFromDB = queryResult[0].password;
+        user.wasFound = true;
+        user.fname = queryResult[0].fname;
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 module.exports = {
-    insertUser,
-    findUser,
-}
+  insertUser,
+  findUser,
+};
