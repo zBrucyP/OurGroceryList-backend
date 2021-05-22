04d4eb2bb6ab4ffb4bc144db1747ea7d2675407a
zBrucyP <zbrucyp@gmail.com>
2021-05-22 17:00:00 -0500

    change log message

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use('/api/lists', authMiddleware.isLoggedIn, lists);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+    console.log(`Listening on ${port}`);
 })
